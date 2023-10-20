ed70e5008b72b2125e2bed2eabd8de27107bb71e
chystarr <chynnastarr2@gmail.com>
2023-10-20 16:37:20 -0400

    Make timeline changes

--- index.ts
+++ index.ts
@@ -1,13 +1,13 @@
 import express, { Express, Request, Response, Router } from 'express';
 import { PrismaClient } from '@prisma/client';
-import timelinesController from './controllers/timelinesController';
+import timelines from './controllers/timelines';
 
 const app: Express = express();
 const port = 8000;
 const router: Router = express.Router();
 const prisma = new PrismaClient();
 
-app.use('/timelines', timelinesController);
+app.use('/timelines', timelines);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World (now in TypeScript)!');
